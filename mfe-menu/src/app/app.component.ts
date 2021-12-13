203c3bf2974f3c71fe5c94d46dca90f6ca0bb171
Sara Cristine Tiedt <sara.tiedt@capco.com>
2021-12-13 15:44:40 -0300

    add navbar to menu

---
+++ mfe-menu/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "mfe-menu-root",
+  template: ` <mfe-menu-menu></mfe-menu-menu> `,
+  styles: [],
+})
+export class AppComponent {
+  title = "mfe-menu";
+}
---
+++ mfe-menu/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
