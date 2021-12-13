1649a96e0dd65087dcf45b40c51e2a5d62bb711d
Sara Cristine Tiedt <sara.tiedt@capco.com>
2021-12-13 16:15:52 -0300

    add EmptyRoute

--- mfe-menu/src/app/app.module.ts
+++ mfe-menu/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -11,11 +12,12 @@ import { APP_BASE_HREF } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    EmptyRouteComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([{path: '**', component: EmptyRouteComponent}])
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
