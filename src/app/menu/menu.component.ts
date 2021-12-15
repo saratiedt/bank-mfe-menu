0e7b27dfe563930a5f9ed0868c7806c15c09dc52
Andrielson Silva <andrielson.silva@capco.com>
2021-12-15 16:20:40 -0300

    Update MenuComponent

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mfe-menu-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  ngOnDestroy() {
+    console.log("Menu OFF!");
+  }
 
   ngOnInit() {
+    console.log("Menu ON!");
   }
 
 }
