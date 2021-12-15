9dafa047af53cb87934e89e1b4f6a3a01dc0d1aa
Andrielson Silva <andrielson.silva@capco.com>
2021-12-15 16:20:22 -0300

    Add MenuService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
+import { startWith } from "rxjs/operators";
+import { MenuService } from "mfe-lib-components";
 
 @Component({
   selector: "mfe-menu-root",
-  template: ` <mfe-menu-menu></mfe-menu-menu> `,
-  styles: [],
+  template: `<ng-container *ngIf="menu$ | async">
+    <mfe-menu-menu></mfe-menu-menu>
+  </ng-container>`,
 })
 export class AppComponent {
   title = "mfe-menu";
+  constructor(private readonly menuService: MenuService) { }
+  menu$ = this.menuService.menu$.pipe(startWith(false));
 }
