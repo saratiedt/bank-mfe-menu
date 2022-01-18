90c4b0ddd6a814dfa981786bdda46789ef736689
Andrielson Silva <andrielson.silva@capco.com>
2022-01-18 09:21:22 -0300

    Refatoração da diretiva

--- src/app/pdpj-link.directive.ts
+++ src/app/pdpj-link.directive.ts
@@ -1,24 +1,28 @@
-import { Directive, ElementRef, HostListener, Inject, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { LOCATION } from './location.token';
+import {
+  Directive,
+  HostListener,
+  Inject,
+  Input,
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { LOCATION } from "./location.token";
 
 @Directive({
-  selector: '[pdpjLink]'
+  selector: "[pdpjLink]",
 })
 export class PdpjLinkDirective {
-
   constructor(
     @Inject(LOCATION)
     private readonly location: Location,
-    private readonly router: Router,
-  ) {
+    private readonly router: Router
+  ) {}
+
+  @Input("pdpjLink") link: string;
+
+  @HostListener("click") onClick() {
+    const prefix = this.link.split("/")[0] || this.link.split("/")[1];
+    return ["app1", "app2", "financial-core", "skeleton"].includes(prefix)
+      ? this.router.navigateByUrl(this.link)
+      : this.location.assign(this.link);
   }
-
-  @Input('pdpjLink') link: string;
-
-  @HostListener('click') onClick() {
-    const prefix = this.link.split('/')[0] || this.link.split('/')[1];
-    return ['app1', 'app2', 'financial-core'].includes(prefix) ? this.router.navigateByUrl(this.link) : this.location.assign(this.link);
-  }
-
 }
